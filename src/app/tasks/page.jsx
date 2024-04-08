df0f7945856f468ea5797c7e70cf7c204a1a71a2
gvicenteprieto <gvicenteprieto@gmail.com>
2024-04-08 01:30:00 -0300

    db

--- src/app/tasks/page.jsx
+++ src/app/tasks/page.jsx
@@ -12,17 +12,19 @@ async function TaskPage() {
   return (
     <main className="flex flex-col items-center justify-between p-10">
       <HeaderPages title={"tasks"} />
+      
       <section className="flex justify-center items-center">
         {tasks.length === 0 ? (
-           <section className="flex flex-col justify-around items-center pt-10">
-           <h3 className="text-2xl font-semi-bold mx-5"> Not Tasks records</h3>
-       
-           </section>
+          <section className="flex flex-col justify-around items-center pt-10">
+            <h3 className="text-2xl font-semi-bold mx-5"> Not Tasks records</h3>
+          </section>
         ) : (
           <Tasks tasks={tasks} />
         )}
       </section>
-      <NavbarAside />
+      <section className="flex flex-col items-center justify-between p-10">
+        <NavbarAside />
+      </section>
     </main>
   );
 }
